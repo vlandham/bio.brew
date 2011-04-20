2eec8366f4f7eecd4f17cade814dbc053cb44a33
Jim Vallandingham <jfv@stowers.org>
2011-04-20 12:53:09 -0500

    pre and post install / remove steps are now done automatically

--- lib/case.sh
+++ lib/case.sh
@@ -2,7 +2,10 @@ case $bb_action in
   "install") 
     if [ $(check_if_installed $seed_name) == "0" ]
     then
+      check_deps ${deps[@]}
+      before_install $seed_name
       do_install
+      after_install $seed_name
     else
       log "recipe already installed, skipping."
     fi
@@ -10,7 +13,9 @@ case $bb_action in
   "remove")
     if [ $(check_if_installed $seed_name) == "1" ]
     then
+      before_remove $seed_name
       do_remove
+      after_remove $seed_name
     else
       log "recipe not installed."
     fi

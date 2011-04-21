3c9f6597cf8c3f361c5e5f5da6eb9ec292f75aec
Jim Vallandingham <jfv@stowers.org>
2011-04-21 11:05:28 -0500

    hack to get list working again

--- lib/list.sh
+++ lib/list.sh
@@ -6,12 +6,13 @@ bb_list()
   then
     for r in `ls $RECIPE_DIR/*.sh`
     do
+      local recipe=`basename $r | sed "s/\.sh$//g"`
       local installed="-"
       local bb_action="list"
       local s_deps=""
       source "${r}"
-
-      [ $(check_if_installed $seed_name) == "1" ] && installed="I" || installed="-"
+      local full_name="$recipe/$seed_name"
+      [ $(check_if_installed $full_name) == "1" ] && installed="I" || installed="-"
       for d in "${deps[@]}"; do s_deps="$s_deps $d"; done
       printf "%s : %-24.24s : %s\n" "$installed" "$seed_name" "$s_deps"
       deps=""

ae5c1e581983588003805f9679c116f3d85e2a4d
Jim Vallandingham <jfv@stowers.org>
2011-04-21 15:58:03 -0500

    updated fastqc. changed to match new organization

--- recipes/fastqc.sh
+++ recipes/fastqc.sh
@@ -1,27 +1,30 @@
 
-local version="0.7.0"
-local URL="http://www.bioinformatics.bbsrc.ac.uk/projects/fastqc/fastqc_v0.7.0.zip"
+local version="0.9.1"
+local URL="http://www.bioinformatics.bbsrc.ac.uk/projects/fastqc/fastqc_v0.9.1.zip"
 local zip_file=`basename $URL`
-local seed_name="fastqc"
+local seed_name="fastqc_$version"
 local unzip_dir="FastQC"
-local deps=(java)
-
+local dep=("java")
 do_install()
 {
-  cd $LOCAL_DIR
+  cd $STAGE_DIR
   log "Downloading"
   curl -sL $URL > ${zip_file}
   log "Unzipping ($zip_file)"
   unzip $zip_file &> $LOG_DIR/${seed_name}.unzip.log.txt
+  mv $unzip_dir $seed_name
   rm -f $zip_file
-  chmod 755 $LOCAL_DIR/$unzip_dir/fastqc
-  ln -s $LOCAL_DIR/$unzip_dir/fastqc ./bin/fastqc
+  cd ..
+  chmod 755 $STAGE_DIR/$seed_name/fastqc
+  ln -s $STAGE_DIR/$seed_name $STAGE_DIR/current
+  ln -s $STAGE_DIR/current/fastqc $LOCAL_DIR/bin/fastqc
 }
 
 do_remove()
 {
-  rm -rf $LOCAL_DIR/$unzip_dir
+  rm -rf $STAGE_DIR/$seed_name
   rm -f $LOCAL_DIR/bin/fastqc
+  rm -f $STAGE_DIR/current
 }
 
 source "$MAIN_DIR/lib/case.sh"

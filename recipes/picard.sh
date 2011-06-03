4ccef4f98d8f9a91aca84e804b7b5788c028860b
Jim Vallandingham <jfv@stowers.org>
2011-06-03 10:55:34 -0500

    updated picard recipe to 1.46. works with new bio.brew

--- recipes/picard.sh
+++ recipes/picard.sh
@@ -1,26 +1,31 @@
 
-local version="1.40"
-local URL="http://sourceforge.net/projects/picard/files/picard-tools/${version}/picard-tools-${version}.zip/download"
-local tb_file=`basename $URL`
-local seed_name="picard"
-local unzip_dir="picard-tools-${version}"
+local version="1.46"
+local type="zip"
+local tb_file="picard-tools-${version}.${type}"
+local URL="http://sourceforge.net/projects/picard/files/picard-tools/${version}/${tb_file}"
+local tb_dir=`basename $tb_file .$type`
+local seed_name="picard_${version}"
 local deps=(java)
 
 do_install()
 {
-  cd $LOCAL_DIR
-  log "Downloading"
-  curl -sL $URL > ${unzip_dir}.zip
-  log "Unzipping"
-  unzip ${unzip_dir}.zip &> $LOG_DIR/${seed_name}.unzip.log.txt
-  rm -f *.zip
-  mv $unzip_dir $seed_name
-  for_env "export PICARD='$LOCAL_DIR/picard'"
+  cd $TB_DIR
+  download $URL $tb_file
+  decompress_tool $tb_file $type
+  mv $tb_dir $seed_name
+  mv $seed_name $STAGE_DIR
+}
+
+do_activate()
+{
+
+  ln -s $STAGE_DIR/$seed_name $STAGE_DIR/current
+  for_env "export PICARD='$STAGE_DIR/current'"
 }
 
 do_remove()
 {
-  rm -rf $LOCAL_DIR/$seed_name
+  remove_recipe $seed_name
 }
 
 source "$MAIN_DIR/lib/case.sh"

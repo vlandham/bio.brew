37b176ec43c5994824a69340d5dd4187a2089995
Jim Vallandingham <jfv@stowers.org>
2011-06-03 12:32:16 -0500

    update gatk to work with new system

--- recipes/gatk.sh
+++ recipes/gatk.sh
@@ -1,23 +1,29 @@
 
-local URL="ftp://ftp.broadinstitute.org/pub/gsa/GenomeAnalysisTK/GenomeAnalysisTK-latest.tar.bz2"
+local version="1.0.5777"
+local type="tar.bz2"
+local URL="ftp://ftp.broadinstitute.org/pub/gsa/GenomeAnalysisTK/GenomeAnalysisTK-${version}.${type}"
 local tb_file=`basename $URL`
-local tb_type="tar.bz2"
-local seed_name="gatk"
+local tb_dir=`basename $tb_file .$type`
+local seed_name="gatk_${version}"
 local deps=("java")
 
 do_install()
 {
   cd $TB_DIR
   download $URL $tb_file
-  decompress_tool $tb_file $tb_type
-  rm -f $tb_file
-  mv GenomeAnalysisTK* ../local/gatk
-  for_env "export GATK='$LOCAL_DIR/gatk'"
+  decompress_tool $tb_file $type
+  mv $tb_dir $STAGE_DIR/$seed_name
+}
+
+do_activate()
+{
+  ln -s $STAGE_DIR/$seed_name $STAGE_DIR/current
+  for_env "export GATK='$STAGE_DIR/current'"
 }
 
 do_remove()
 {
-  rm -rf $LOCAL_DIR/gatk
+  rm -rf $STAGE_DIR/$seed_name
 }
 
 source "$MAIN_DIR/lib/case.sh"

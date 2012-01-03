760e608c05de008552464b3981e31e59451dfead
Jim V <jfv@stowers.org>
2012-01-03 10:44:17 -0600

    bump to 1.3.0

--- recipes/cufflinks.sh
+++ recipes/cufflinks.sh
@@ -1,4 +1,4 @@
-local version="1.0.3"
+local version="1.3.0"
 local seed_name="cufflinks_$version"
 local type="tar.gz"
 local URL="http://cufflinks.cbcb.umd.edu/downloads/cufflinks-${version}.Linux_x86_64.${type}"

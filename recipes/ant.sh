d409b36d5d017dcbcee513351deded4ea3609057
Jim V <jfv@stowers.org>
2012-01-04 11:22:19 -0600

    update ant recipe for versioning

--- recipes/ant.sh
+++ recipes/ant.sh
@@ -1,25 +1,34 @@
-
-local version="1.8.1"
-local URL="http://mirrors.kahuki.com/apache//ant/binaries/apache-ant-${version}-bin.tar.gz"
+local version="1.8.2"
+local type="tar.gz"
+local URL="http://mirrors.kahuki.com/apache//ant/binaries/apache-ant-${version}-bin.${type}"
 local tb_file=`basename $URL`
-local tb_type="tar.gz"
-local seed_name="ant"
+local seed_name="apache-ant-${version}"
 local deps=(java)
 local root_seed="apache-ant-${version}"
 
 do_install()
 {
-  cd $LOCAL_DIR
+  cd $TB_DIR
   download $URL $tb_file
-  decompress_tool $tb_file $tb_type
+  decompress_tool $tb_file $type
+  mv $seed_name $STAGE_DIR
+}
+
+do_activate()
+{
   clear_env
-  for_env " export PATH=\"$LOCAL_DIR/$root_seed/bin:\$PATH\" "
-  for_env " export ANT_HOME='$LOCAL_DIR/$root_seed'"
+  for_env " export PATH=\"$STAGE_DIR/$seed_name/bin:\$PATH\" "
+  for_env " export ANT_HOME='$STAGE_DIR/$seed_name'"
+}
+
+do_test()
+{
+  log "test"
 }
 
 do_remove()
 {
-  rm -rf $LOCAL_DIR/$root_seed*
+  rm -rf $STAGE_DIR/$seed_name*
 }
 
 source "$MAIN_DIR/lib/case.sh"

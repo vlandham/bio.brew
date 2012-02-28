6484b11f98b988013bafa8420e25dd71855ddfac
Jim V <jfv@stowers.org>
2012-02-28 15:14:53 -0600

    updatae snpeff

--- recipes/snpeff.sh
+++ recipes/snpeff.sh
@@ -1,10 +1,10 @@
 
-local version="2_0_4_rc1"
+local version="2_0_5d"
 local type="zip"
-local tb_file="snpEff_v${version}.${type}"
+local tb_file="snpEff_v${version}_core.${type}"
 local URL="http://sourceforge.net/projects/snpeff/files/${tb_file}"
 local tb_dir=`basename $tb_file .$type`
-local seed_name="snpeff_${version}"
+local seed_name="snpEff_${version}"
 local deps=(java)
 
 do_install()
@@ -12,7 +12,7 @@ do_install()
   cd $TB_DIR
   download $URL $tb_file
   decompress_tool $tb_file $type
-  mv $tb_dir $seed_name
+  # mv $tb_dir $seed_name
   mv $seed_name $STAGE_DIR
 }
 

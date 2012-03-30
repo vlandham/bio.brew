f1acf481203630f21fe33e2de43f48a87e52d89c
Jim V <jfv@stowers.org>
2012-03-30 10:06:46 -0500

    update version. add comment

--- recipes/bamtools.sh
+++ recipes/bamtools.sh
@@ -1,4 +1,18 @@
-local version="1.0.2"
+# I cannot find any documentation about 
+# where the version of bamtools is located and when it changes.
+# Nor does it look like they are currently releasing versioned packages
+# of their code (which is unfortunate and lame). 
+# 
+# BUT bamtools does have a version associated with it
+# bamtools version produces it.
+#
+# Best way I've found so far to deal with this is
+# bb install bamtools from github, then look at the version, then bb remove,
+#  change the version number in the recipe, and bb install again
+#
+# (not quite as aweful as it sounds - but pretty close).
+# local version="1.0.2"
+local version="2.1.1"
 local URL="https://github.com/pezmaster31/bamtools"
 local seed_name="bamtools_$version"
 local deps=()

87c6dd2b9f17a4b28fd84feec4b1aa7054a900bc
Jim V <jfv@stowers.org>
2012-03-30 14:34:48 -0500

    fix seed name. untested

--- recipes/breakway.sh
+++ recipes/breakway.sh
@@ -1,22 +1,24 @@
 
 local version="0.7"
-local URL="http://sourceforge.net/projects/breakway/files/breakway.${version}/breakway.${version}.tar.gz/download"
-local tb_type="tar.gz"
-local seed_name="breakway"
+local type="tar.gz"
+local URL="http://sourceforge.net/projects/breakway/files/breakway.${version}/breakway.${version}.${type}/download"
 local deps=("perl-5.12.2" "p5_Math_CDF")
-local root_seed="breakway.${version}"
-local tb_file=${root_seed}.${tb_type}
+local seed_name="breakway-${version}"
+local tb_file=`basename $URL`
 local install_files=(breakway.run.pl scripts/breakway.parameters.pl)
 
 do_install()
 {
-  cd $LOCAL_DIR
-  log "Downloading ${root_seed}"
-  curl -sL $URL > $tb_file
-  decompress_tool $tb_file $tb_type
-  rm -f $tb_file
-  cd $root_seed
-  link_from_stage $root_seed ${install_files[@]} 
+  cd $TB_DIR
+  download $URL $tb_file
+  decompress_tool $tb_file $type
+  mv `basename $tb_file $type` $STAGE_DIR/$seed_name
+  cd $STAGE_DIR/$seed_name
+}
+
+do_activate()
+{
+  link_from_stage $seed_name ${install_files[@]}
 }
 
 do_remove()

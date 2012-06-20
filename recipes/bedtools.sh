aebbadff08680eb53b413d210475ba7d93bb9b6d
Jim V <jfv@stowers.org>
2012-06-20 16:24:54 -0500

    fix bedtools seed name. fix bins to install

--- recipes/bedtools.sh
+++ recipes/bedtools.sh
@@ -2,25 +2,52 @@ local version="2.15.0"
 local type="tar.gz"
 local URL="http://bedtools.googlecode.com/files/BEDTools.v${version}.${type}"
 local tb_file=`basename $URL`
-local seed_name="BEDTools-Version-${version}"
-local install_files=(bin/coverageBed bin/slopBed bin/linksBed 
-bin/mergeBed bin/bed12ToBed6 bin/closestBed bin/complementBed 
-bin/overlap bin/annotateBed bin/subtractBed bin/maskFastaFromBed 
-bin/windowBed bin/sortBed bin/unionBedGraphs bin/genomeCoverageBed 
-bin/pairToPair bin/fastaFromBed bin/bedToBam bin/pairToBed 
-bin/flankBed bin/bedToIgv bin/intersectBed bin/shuffleBed 
-bin/fjoin bin/cuffToTrans bin/bamToBed bin/multiIntersectBed 
-bin/multiBamCov bin/tabBam bin/nucBed bin/tagBam bin/nuclBed bin/groupBy)
+local tar_name="BEDTools-Version-${version}"
+local seed_name="bedtools-${version}"
+local install_files=(bin/annotateBed
+bin/bamToBed
+bin/bed12ToBed6
+bin/bedToBam
+bin/bedToIgv
+bin/bedpeToBam
+bin/bedtools
+bin/closestBed
+bin/clusterBed
+bin/complementBed
+bin/coverageBed
+bin/fastaFromBed
+bin/flankBed
+bin/genomeCoverageBed
+bin/getOverlap
+bin/groupBy
+bin/intersectBed
+bin/linksBed
+bin/maskFastaFromBed
+bin/mergeBed
+bin/multiBamCov
+bin/multiIntersectBed
+bin/nucBed
+bin/pairToBed
+bin/pairToPair
+bin/shuffleBed
+bin/slopBed
+bin/sortBed
+bin/subtractBed
+bin/tagBam
+bin/unionBedGraphs
+bin/windowBed
+bin/windowMaker)
+
 
 do_install()
 {
   cd $TB_DIR
   download $URL $tb_file
   decompress_tool $tb_file $type
-  cd $seed_name
+  mv $tar_name $STAGE_DIR/$seed_name
+  cd $STAGE_DIR/$seed_name
   make_tool $seed_name $make_j
   cd ..
-  mv $seed_name $STAGE_DIR
 }
 
 do_activate()

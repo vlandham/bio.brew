7cd7b24315460b6b39f41ad833f7a8fd87b238fb
Jim V <jfv@stowers.org>
2012-06-21 13:55:30 -0500

    use perlbrew for perl install now

--- recipes/perl.sh
+++ recipes/perl.sh
@@ -1,41 +1,20 @@
-
-local version="5.14.2"
-local type="tar.gz"
-local URL="http://www.cpan.org/src/5.0/perl-${version}.${type}"
-local tb_file=`basename $URL`
+local version="5.16.0"
 local seed_name="perl-${version}"
-local install_files=(bin/perl bin/cpan)
+local deps=(perlbrew)
 
 do_install()
 {
-  cd $TB_DIR
-  download $URL $tb_file
-  decompress_tool $tb_file $type
-  mv $seed_name $STAGE_DIR
-  cd $STAGE_DIR/$seed_name
-  log "Configuring perl"
-  # sh Configure -de -Dprefix=$STAGE_DIR/$seed_name &> /dev/null
-  sh Configure -de -Dprefix=$LOCAL/$seed_name &> /dev/null
-  make_tool $seed_name $make_j
-  install_tool $seed_name
+  perlbrew install $seed_name
 }
 
 do_activate()
 {
-  link_from_stage $seed_name ${install_files[@]}
-  link_library $seed_name
-  link_man $seed_name
-  for_env "export PATH='$STAGE_DIR/current/bin:$PATH'"
-
-  # setup_cpan_config
-
+  perlbrew switch $seed_name
 }
 
 do_remove()
 {
-  unlink_library $seed_name
-  remove_recipe $seed_name
-  remove_from_stage $seed_name ${install_files[@]}
+  log "use perlbrew to manage perl install"
 }
 
 source "$MAIN_DIR/lib/case.sh"

d6046422973eb33f47e2bb4eec1c13cd5467002f
Jim V <jfv@stowers.org>
2012-06-21 15:15:13 -0500

    clean

--- recipes/meme.sh
+++ recipes/meme.sh
@@ -1,11 +1,9 @@
 local version="4.7.0"
 local type="tar.gz"
-# sourceforge mirror was found by copying download link in 
-# Firefox's download manager.
 local URL="http://meme.nbcr.net/downloads/meme_${version}.${type}"
 local tb_file=`basename $URL`
 local seed_name="meme_${version}"
-local install_files=(samtools misc/samtools.pl bcftools/bcftools bcftools/vcfutils.pl)
+# local install_files=(samtools misc/samtools.pl bcftools/bcftools bcftools/vcfutils.pl)
 
 do_install()
 {
@@ -17,15 +15,14 @@ do_install()
   make_tool $seed_name $make_j
   cd ..
   mv $seed_name $STAGE_DIR
-  cd $STAGE_DIR
-  cd $seed_name
+  cd $STAGE_DIR/$seed_name
   install_tool $seed_name
 }
 
 do_activate()
 {
   switch_current $seed_name
-  for_env "PATH='$STAGE_DIR/current/bin:$PATH'"
+  for_env "export PATH=\"$STAGE_DIR/current/bin:\$PATH\""
 }
 
 do_test()

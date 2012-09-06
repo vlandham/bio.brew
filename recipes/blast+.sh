421cb9df0eac942ab2ae67f8ac212c768444ae35
Madelaine Gogol <madelaine@gmail.com>
2012-09-06 13:19:35 -0500

    finished blast+ recipe

--- recipes/blast+.sh
+++ recipes/blast+.sh
@@ -5,7 +5,7 @@ local URL="ftp://ftp.ncbi.nlm.nih.gov/blast/executables/${tool_name}/${version}/
 local tb_file=`basename $URL`
 local tb_name="ncbi-blast-${version}+-src"
 local seed_name="blast+_${version}"
-local install_files=()
+local install_files=(c++/ReleaseMT/bin/blastdb_aliastool c++/ReleaseMT/bin/blastdbcmd c++/ReleaseMT/bin/blast_formatter c++/ReleaseMT/bin/blastp c++/ReleaseMT/bin/convert2blastmask c++/ReleaseMT/bin/deltablast c++/ReleaseMT/bin/gene_info_reader c++/ReleaseMT/bin/makeblastdb c++/ReleaseMT/bin/makeprofiledb c++/ReleaseMT/bin/psiblast c++/ReleaseMT/bin/rpstblastn c++/ReleaseMT/bin/seqdb_demo c++/ReleaseMT/bin/tblastn c++/ReleaseMT/bin/test_pcre c++/ReleaseMT/bin/windowmasker c++/ReleaseMT/bin/blastdbcheck c++/ReleaseMT/bin/blastdbcp c++/ReleaseMT/bin/blastn c++/ReleaseMT/bin/blastx c++/ReleaseMT/bin/datatool c++/ReleaseMT/bin/dustmasker c++/ReleaseMT/bin/legacy_blast.pl c++/ReleaseMT/bin/makemc++/ReleaseMT/bindex c++/ReleaseMT/bin/project_tree_builder c++/ReleaseMT/bin/rpsblast c++/ReleaseMT/bin/segmasker c++/ReleaseMT/bin/seqdb_perf c++/ReleaseMT/bin/tblastx c++/ReleaseMT/bin/update_blastdb.pl c++/ReleaseMT/bin/windowmasker_2.2.22_adapter.py)
 
 do_install()
 {

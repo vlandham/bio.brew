fd7140326cc6b522ffd207d1a395eedc2d05f11a
Jim Vallandingham <jfv@stowers.org>
2012-10-18 12:24:13 -0500

    update R

--- recipes/r.sh
+++ recipes/r.sh
@@ -1,4 +1,4 @@
-local URL="http://cran.stat.ucla.edu/src/base/R-2/R-2.12.0.tar.gz"
+local URL="http://www.cran.r-project.org/src/base/R-2/R-2.15.0.tar.gz"
 local tb_file=`basename $URL`
 local type="tar.gz"
 local seed_name=$(extract_tool_name $tb_file $type)
@@ -10,7 +10,7 @@ do_install()
   download $URL $tb_file
   decompress_tool $tb_file $type
   cd $seed_name
-  configure_tool $seed_name "--with-x=no"
+  configure_tool $seed_name "--enable-R-shlib --enable-memory-profiling --enable-static --enable-shared"
   make_tool $seed_name $make_j
   install_tool $seed_name
   link_from_stage $seed_name ${install_files[@]}

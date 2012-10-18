bcf0039041d14fbd163355f479c94932e292512f
Jim Vallandingham <jfv@stowers.org>
2012-10-18 12:22:25 -0500

    fix nodejs recipe using precompiled binaries

--- recipes/node.sh
+++ recipes/node.sh
@@ -1,18 +1,34 @@
-local version="0.5.0-pre"
-local URL="https://github.com/joyent/node.git"
-local seed_name="node_$version"
-local deps=()
-local install_files=(node)
+# now uses pre-compiled binaries, to get things working
+local version="0.8.12"
+local type="tar.gz"
+local URL="http://nodejs.org/dist/v${version}/node-v${version}-linux-x64.${type}"
+# local URL="http://nodejs.org/dist/v${version}/node-v${version}.${type}"
+local tb_file=`basename $URL`
+local tb_directory=`basename $URL .${type}`
+local seed_name="node-${version}"
+local install_files=(bin/node bin/node-waf bin/npm)
 
 do_install()
 {
-  cd $STAGE_DIR
-  download_git $URL $seed_name
-  cd $seed_name
-  configure_tool $seed_name 
-  make_tool $seed_name $make_j
-  link_from_stage $recipe ${install_files[@]}
+  cd $TB_DIR
+  download $URL $tb_file
+  decompress_tool $tb_file $type
+  mv $tb_directory $STAGE_DIR/$seed_name
+  cd $STAGE_DIR/$seed_name
+  # make_tool $seed_name $make_j
+  # cd ..
 }
+
+do_activate()
+{
+  link_from_stage $seed_name ${install_files[@]}
+}
+
+do_test()
+{
+  log "test"
+}
+
 
 do_remove()
 {

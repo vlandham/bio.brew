e9c3066208f37d59fd22754c82f9bdf8d4b3c9bd
Jim Vallandingham <jfv@stowers.org>
2012-11-16 15:13:24 -0600

    threaded version of perl

--- recipes/perl.sh
+++ recipes/perl.sh
@@ -1,15 +1,17 @@
-local version="5.16.0"
+local version="5.16.1"
 local seed_name="perl-${version}"
+local threaded_seed_name="perl-${version}t"
 local deps=(perlbrew)
 
 do_install()
 {
   perlbrew --force install $seed_name
+  perlbrew --force install $seed_name -Dusethreads --as ${threaded_seed_name}
 }
 
 do_activate()
 {
-  perlbrew switch $seed_name
+  perlbrew switch $threaded_seed_name
 }
 
 do_remove()

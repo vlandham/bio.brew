79ed6bac6cc939bdb0293439eab6ee68c8d48e7e
Jim Vallandingham <jfv@stowers.org>
2013-06-12 08:54:59 -0500

    update gatk recipe. sucks that you have to sign in...

--- recipes/gatk.sh
+++ recipes/gatk.sh
@@ -1,18 +1,24 @@
 
 #local version="1.1-37-ge63d9d8"
 # local version="1.2-62-g41ddc7b"
-local version="1.4-37-g0b29d54"
+# local version="1.4-37-g0b29d54"
+local version="2.4-9"
+local real_version="2.4-9-g532efad"
 local type="tar.bz2"
-local URL="ftp://ftp.broadinstitute.org/pub/gsa/GenomeAnalysisTK/GenomeAnalysisTK-${version}.${type}"
-local tb_file=`basename $URL`
-local tb_dir=`basename $tb_file .$type`
+
+# as you have to sign in and agree to statement 
+# right now the Tarball is expected to just already 
+# be in the TARBALL directory
+# local URL="ftp://ftp.broadinstitute.org/pub/gsa/GenomeAnalysisTK/GenomeAnalysisTK-${version}.${type}"
+local tb_file="GenomeAnalysisTK-${version}.${type}"
+local tb_dir="GenomeAnalysisTK-${real_version}"
 local seed_name="gatk_${version}"
 local deps=("java")
 
 do_install()
 {
   cd $TB_DIR
-  download $URL $tb_file
+  # download $URL $tb_file
   decompress_tool $tb_file $type
   mv $tb_dir $STAGE_DIR/$seed_name
 }

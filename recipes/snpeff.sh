b48b055e996a1b8a4a66f3b4657c85d814d4d8b8
Jim Vallandingham <jfv@stowers.org>
2013-06-21 14:34:48 -0500

    update snpeff to 3.3

--- recipes/snpeff.sh
+++ recipes/snpeff.sh
@@ -1,5 +1,5 @@
 
-local version="3_1"
+local version="3_3"
 local type="zip"
 local tb_file="snpEff_v${version}_core.${type}"
 local URL="http://sourceforge.net/projects/snpeff/files/${tb_file}"
@@ -14,18 +14,19 @@ do_install()
   cd $TB_DIR
   download $URL $tb_file
   decompress_tool $tb_file $type
+  mv snpEff $STAGE_DIR/$seed_name
   # mv $tb_dir $seed_name
-  mv $seed_name $STAGE_DIR
+  #mv $seed_name $STAGE_DIR
   cd $STAGE_DIR/$seed_name
   # TODO: make this into an array and iterate
   # stop being so lazy
-  java -jar ./snpEff.jar download WS220.66
-  java -jar ./snpEff.jar download Zv9.66
 }
 
 do_activate()
 {
   ln -s $STAGE_DIR/$seed_name $STAGE_DIR/current
+  java -jar ./snpEff.jar download WS220.66
+  java -jar ./snpEff.jar download Zv9.66
   for_env "export SNPEFF='$STAGE_DIR/current'"
 }
 
